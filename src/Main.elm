20b9d63da381ba6e8021598c19a7ea0c921699a7
youcefs21 <youcefs2121@gmail.com>
2022-03-10 08:17:20 -0500

    basic state set up

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,47 @@
 module Main exposing (..)
 
-import Html exposing (..)
+import GraphicSVG exposing (..)
+import GraphicSVG.App exposing (..)
 
-main =
-  text "Hello World!"
+
+myShapes model = 
+  case model.state of
+    Caf -> [
+
+      ]
+
+
+
+type State = Caf
+
+
+type Msg = Tick Float GetKeyState
+
+type alias Model = { 
+    time  : Float , 
+    state : State 
+  }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Tick t _ ->
+            model
+
+init = {
+    time = 0,
+    state = Caf
+  }
+
+
+main = 
+  gameApp Tick { 
+    model  = init, 
+    title  = "The Best Rocket Game of 1XD3 by Lab 3 Group 2",
+    update = update,
+    view   = view       
+  }
+
+
+view model = collage 192 128 ( myShapes model )
